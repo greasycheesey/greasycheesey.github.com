6c6d11a53d7a3470d318182d2e1510d3f994fc50
Al <128782773+greasycheesey@users.noreply.github.com>
2024-11-08 15:05:00 +0700

    hmmm

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const purchaseBanana = document.getElementById("bananaButton");
 const bananaPurchasedText = document.getElementById("bananaPurchased");
+const magicButton = document.getElementById("magicwowzers")
 let bananaPurchased = 0;
 
 purchaseBanana.onclick = function() {
     bananaPurchased++;
     bananaPurchasedText.innerHTML = `you have bought ${bananaPurchased} bananas.`;
 };
+
+magicButton.onclick = function() {
+    const bunny = document.createElement("i")
+    document.body.appendChild("greasycheesey/greasycheesey.github.com/stuff/hmm.png")
+
+}
